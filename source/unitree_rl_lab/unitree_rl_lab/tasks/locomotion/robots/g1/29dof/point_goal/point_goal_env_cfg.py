85af1b541accd06397862ad5a34d25c44affa71d
luoyanning <luoyanning@luoyanningdeMacBook-Air.local>
2026-03-17 11:48:50 +0800

    implement G1 left-hand touch ball task

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/g1/29dof/point_goal/point_goal_env_cfg.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/g1/29dof/point_goal/point_goal_env_cfg.py
@@ -22,10 +22,10 @@ from .point_goal_mdp import (
 )
 
 
-SUCCESS_DISTANCE = 0.20
-SUCCESS_HOLD_STEPS = 10
+SUCCESS_DISTANCE = 0.25
+SUCCESS_HOLD_STEPS = 5
 STOP_VELOCITY_THRESHOLD = 0.15
-STOP_YAW_RATE_THRESHOLD = 0.25
+STOP_YAW_RATE_THRESHOLD = 0.35
 
 
 @configclass
@@ -34,25 +34,28 @@ class RobotPointGoalEnvCfg(RobotEnvCfg):
         super().__post_init__()
 
         self.episode_length_s = 12.0
+        self.scene.terrain.terrain_type = "plane"
+        self.scene.terrain.terrain_generator = None
         self.commands.base_velocity = PointGoalCommandCfg(
             asset_name="robot",
             resampling_time_range=(self.episode_length_s, self.episode_length_s),
             debug_vis=True,
-            radius_range=(0.4, 1.0),
-            angle_range=(-0.5235987755982988, 0.5235987755982988),
-            forward_gain=1.0,
-            lateral_gain=0.4,
-            heading_gain=0.6,
+            radius_range=(0.35, 0.8),
+            angle_range=(-0.17453292519943295, 0.17453292519943295),
+            forward_gain=0.8,
+            lateral_gain=0.0,
+            heading_gain=0.2,
             min_lin_vel_x=0.0,
-            max_lin_vel_x=0.45,
-            max_lin_vel_y=0.12,
-            max_ang_vel_z=0.45,
-            slow_down_distance=1.2,
+            max_lin_vel_x=0.35,
+            max_lin_vel_y=0.0,
+            max_ang_vel_z=0.15,
+            slow_down_distance=1.0,
             stop_distance=0.35,
             heading_slow_down_distance=0.6,
-            turn_in_place_threshold=0.60,
+            turn_in_place_threshold=1.20,
             target_height_offset=0.03,
         )
+        self.events.reset_robot_joints.params["velocity_range"] = (0.0, 0.0)
 
         self.observations.policy.point_goal_target_world = ObsTerm(
             func=point_goal_target_pos_env,
@@ -94,35 +97,35 @@ class RobotPointGoalEnvCfg(RobotEnvCfg):
             params={"command_name": "base_velocity"},
         )
 
-        self.rewards.track_lin_vel_xy.weight = 0.80
-        self.rewards.track_ang_vel_z.weight = 0.35
-        self.rewards.alive.weight = 0.12
+        self.rewards.track_lin_vel_xy.weight = 1.00
+        self.rewards.track_ang_vel_z.weight = 0.20
+        self.rewards.alive.weight = 0.15
         self.rewards.action_rate = None
-        self.rewards.gait.weight = 0.30
-        self.rewards.feet_clearance.weight = 0.40
+        self.rewards.gait.weight = 0.40
+        self.rewards.feet_clearance.weight = 0.50
         self.rewards.joint_deviation_arms.weight = -0.05
-        self.rewards.joint_deviation_waists.weight = -0.50
-        self.rewards.joint_deviation_legs.weight = -0.50
-        self.rewards.flat_orientation_l2.weight = -8.0
-        self.rewards.base_height.weight = -15.0
-        self.rewards.undesired_contacts.weight = -2.0
+        self.rewards.joint_deviation_waists.weight = -0.80
+        self.rewards.joint_deviation_legs.weight = -0.80
+        self.rewards.flat_orientation_l2.weight = -10.0
+        self.rewards.base_height.weight = -18.0
+        self.rewards.undesired_contacts.weight = -3.0
         self.rewards.goal_progress = RewTerm(
             func=point_goal_progress_reward,
-            weight=10.0,
+            weight=6.0,
             params={
                 "command_name": "base_velocity",
                 "success_distance": SUCCESS_DISTANCE,
                 "success_hold_steps": SUCCESS_HOLD_STEPS,
                 "stop_velocity_threshold": STOP_VELOCITY_THRESHOLD,
                 "stop_yaw_rate_threshold": STOP_YAW_RATE_THRESHOLD,
-                "clip_value": 0.08,
-                "positive_scale": 3.0,
-                "regress_scale": 2.0,
+                "clip_value": 0.06,
+                "positive_scale": 2.0,
+                "regress_scale": 1.5,
             },
         )
         self.rewards.goal_completion = RewTerm(
             func=point_goal_completion_reward,
-            weight=6.0,
+            weight=4.0,
             params={
                 "command_name": "base_velocity",
                 "success_distance": SUCCESS_DISTANCE,
@@ -134,7 +137,7 @@ class RobotPointGoalEnvCfg(RobotEnvCfg):
         )
         self.rewards.goal_distance = RewTerm(
             func=point_goal_distance_reward,
-            weight=3.0,
+            weight=2.0,
             params={
                 "command_name": "base_velocity",
                 "success_distance": SUCCESS_DISTANCE,
@@ -146,7 +149,7 @@ class RobotPointGoalEnvCfg(RobotEnvCfg):
         )
         self.rewards.goal_stop = RewTerm(
             func=point_goal_stop_reward,
-            weight=4.0,
+            weight=2.0,
             params={
                 "command_name": "base_velocity",
                 "success_distance": SUCCESS_DISTANCE,
@@ -158,7 +161,7 @@ class RobotPointGoalEnvCfg(RobotEnvCfg):
         )
         self.rewards.goal_success = RewTerm(
             func=point_goal_success_bonus,
-            weight=30.0,
+            weight=20.0,
             params={
                 "command_name": "base_velocity",
                 "success_distance": SUCCESS_DISTANCE,

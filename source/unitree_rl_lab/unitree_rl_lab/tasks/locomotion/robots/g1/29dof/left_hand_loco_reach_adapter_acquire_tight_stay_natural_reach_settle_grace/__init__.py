62906f40412cf2a3478969fd15a69f810f4e9cf2
luoyanning <shackleadriana@gmail.com>
2026-03-25 15:42:35 +0800

    implement G1 left-hand touch ball task

---
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/g1/29dof/left_hand_loco_reach_adapter_acquire_tight_stay_natural_reach_settle_grace/__init__.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+
+
+gym.register(
+    id="Unitree-G1-29dof-LeftHand-LocoReach-AdapterAcquireTightStay-NaturalReachSettleGrace-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": (
+            f"{__name__}.left_hand_loco_reach_adapter_acquire_tight_stay_natural_reach_settle_grace_env_cfg:"
+            "RobotLeftHandLocoReachAdapterAcquireTightStayNaturalReachSettleGraceEnvCfg"
+        ),
+        "play_env_cfg_entry_point": (
+            f"{__name__}.left_hand_loco_reach_adapter_acquire_tight_stay_natural_reach_settle_grace_env_cfg:"
+            "RobotLeftHandLocoReachAdapterAcquireTightStayNaturalReachSettleGracePlayEnvCfg"
+        ),
+        "rsl_rl_cfg_entry_point": f"{__name__}.rsl_rl_ppo_cfg:LeftHandLocoReachAdapterAcquireTightStayNaturalReachSettleGracePPORunnerCfg",
+    },
+)
